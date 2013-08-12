2751b840a602bb28d1eb83ab87be097a694931d4
Yrom Wang <yrom@qq.com>
2013-08-12 14:32:29 +0800

    refactor IDataSource

--- DanmakuFlameMaster/src/main/java/master/flame/danmaku/danmaku/parser/android/JSONObjectSource.java
+++ DanmakuFlameMaster/src/main/java/master/flame/danmaku/danmaku/parser/android/JSONObjectSource.java
@@ -13,6 +13,7 @@ import master.flame.danmaku.danmaku.util.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.net.Uri;
 import android.text.TextUtils;
 
 /**
@@ -25,19 +26,36 @@ public class JSONObjectSource implements IDataSource<JSONObject>{
 	public JSONObjectSource(String json) throws JSONException{
 		init(json);
 	}
+	
 	public JSONObjectSource(InputStream in) throws JSONException{
+		init(in);
+	}
+	
+	private void init(InputStream in) throws JSONException {
 		if(in == null)
 			throw new NullPointerException("input stream cannot be null!");
 		mInput = in;
 		String json = IOUtils.getString(mInput);
 		init(json);
 	}
+	
 	public JSONObjectSource(URL url) throws JSONException, IOException{
 		this(url.openStream());
 	}
+	
 	public JSONObjectSource(File file) throws FileNotFoundException, JSONException{
-		this(new FileInputStream(file));
+		init(new FileInputStream(file));
 	}
+	
+	public JSONObjectSource(Uri uri) throws IOException, JSONException {
+		String scheme = uri.getScheme();
+        if (SCHEME_HTTP_TAG.equalsIgnoreCase(scheme) || SCHEME_HTTPS_TAG.equalsIgnoreCase(scheme)) {
+            init(new URL(uri.getPath()).openStream());
+        } else if (SCHEME_FILE_TAG.equalsIgnoreCase(scheme)) {
+            init(new FileInputStream(uri.getPath()));
+        }
+	}
+	
 	private void init(String json) throws JSONException {
 		if(!TextUtils.isEmpty(json)){
 			mJSONObject = new JSONObject(json);
