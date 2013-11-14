47792cfc7168499c35a7d74b83b4f7303558c611
calmer <calmer91@gmail.com>
2013-11-14 19:53:53 +0800

    调整工程结构

---
+++ Sample/src/main/java/com/sample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.sample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
